eccbcc3a4f6e9b881f1a56fb8fd0c455671ddced
celsve-cmd <celsve@gmail.com>
2026-02-27 15:05:05 +0100

    -

--- view.js
+++ view.js
@@ -4,6 +4,12 @@ function updateView() {
 
     document.getElementById('krokodillespill').innerHTML = /*HTML*/ `
     
+    <h1>Krokodillespillet!</h1>
+    <p>Skriv inn enten <, > eller = ettersom hva tallene viser!</p>
+    <p>Er første tall større enn andre tall, da må du skrive ></p>
+    <p>Er andre tall større enn første tall, da må du skrive <</p>
+    <p>Er tallene like store, må du skrive =</p>
+
     <div id="points">Points: ${points}</div>
     <br>
     <img src="ai-generated-9314391_640-removebg-preview.png" width="30%", height="30%">
